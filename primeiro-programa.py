20ec0213e57b4546b8235e3ca5971c5af4b4302e
mauricio-py <mauricioamaral.trabalho@gmail.com>
2024-09-20 18:25:05 -0300

    Update primeiro-programa.py
    Uma atualização para testar uso de função e estrutura de repetição

--- primeiro-programa.py
+++ primeiro-programa.py
@@ -1,10 +1,24 @@
-cont = int(input("Digite quantas notas o aluno tem: "))
-nota = 0
-soma = 0
+opcao = False
 
-for i in range(0, cont):
-    nota = float(input(f"Digite a primeiro nota {i + 1}: "))
-    soma += nota
+def media():
+  notas = 0
+  quantNotas = int(input("\nQuantas notas você deseja calcular a média? "))
+  for i in range(0, quantNotas, 1):
+    nota = int(input(f"Digite a nota {i + 1}ª: "))
+    notas += nota
+    print(notas)
+  print(quantNotas)  
+  media = notas / quantNotas
+  print(f"\n a media é {media} \n")
 
-media = soma / cont
-print(media)
+while True:
+  opcao = int(input("""[ 1 ] fazer a media de aluno
+[ 0 ] sair do programa \n
+  """))
+  
+  if opcao == 1:
+    media()
+  
+  
+  if opcao == 0:
+    break
